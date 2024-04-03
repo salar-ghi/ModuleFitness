a8e74e0f0aa411741bc391b4296ad444385b523f
salar-ghi <salar.ghi1993@gmail.com>
2024-04-04 01:51:16 +0330

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = (await import("@ducanh2912/next-pwa")).default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable : false,
+    workboxOptions: {
+        disableDevLogs : true,
+    }
+});
+
+
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+// export default nextConfig;
+export default withPWA(nextConfig);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,29 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: 'jit',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
+      colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				light: '#F6F6F6',
+				dark: '#0e7490',
+				'light-green': '#c9e4db',
+				'tint-green': '#6eb79e',
+				'primary-green': '#4AA586',
+				'hover-green': '#047857'
+			},
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +31,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+    require('tailwind-scrollbar-hide'),
+    require("flowbite/plugin")
+  ],
 };
 export default config;
